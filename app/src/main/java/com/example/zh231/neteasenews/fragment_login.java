80c3ef0457aec922299621a67fc72860ed734aac
2314372037 <zh2314372037@outlook.com>
2019-03-28 23:52:15 +0800

    增加登录页面

---
+++ app/src/main/java/com/example/zh231/neteasenews/fragment_login.java
@@ -0,0 +1,26 @@
+package com.example.zh231.neteasenews;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class fragment_login extends Fragment {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_fragment_login,container,false);
+
+
+        return view;
+    }
+}

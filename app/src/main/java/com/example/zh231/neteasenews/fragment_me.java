2e372d0a47ab9ae8efb0659fd2b32e0c4a9f82ea
2314372037 <zh2314372037@outlook.com>
2019-03-30 00:42:02 +0800

    准备接入登录

--- app/src/main/java/com/example/zh231/neteasenews/fragment_me.java
+++ app/src/main/java/com/example/zh231/neteasenews/fragment_me.java
@@ -133,9 +133,6 @@ public class fragment_me extends Fragment implements View.OnClickListener{
                     me_content.setVisibility(View.VISIBLE);
                     me_login.setVisibility(View.INVISIBLE);
                     Toast.makeText(getContext(),"登录成功",Toast.LENGTH_SHORT).show();
-//                    getFragmentManager().beginTransaction().hide(this).commit();
-//                    getFragmentManager().beginTransaction().remove(this).commit();
-//                    getFragmentManager().beginTransaction().replace(R.id.fragment_frameLayout,fm).commit();
                 }
                 break;
             case R.id.forget_psd://忘记密码
@@ -155,5 +152,16 @@ public class fragment_me extends Fragment implements View.OnClickListener{
 
                 break;
         }
+    }
+
+
+    private void loginForWeibo(){//微博登录
+        /*说明:访问url1获取请求头"Location"，访问获取到的Location获取请求头SINA-LB和SINA-TS，保存这两值(Location为微博登录api网页地址)，（未知:请求Location需要带一个Cookie，内容为SCF=xxxxx;SUB=xxxx）
+          输入账号信息后点登录,然后请求https://api.weibo.com/oauth2/authorize，请求头Cookie:为之前的SCF=xxxxx;SUB=xxxx，保存响应头Set-Cookie,获取请求头Location，访问并获取请求头Set-Cookie保存
+          (未知:访问时注意带_ntes_nnid)，后面的请求Cookie带_ntes_nnid=请求
+         */
+        String url1="https://reg.163.com/outerLogin/oauth2/connect.do?target=3&url=https://3g.163.com/touch/all?ver=c&url2=" +
+                "https://3g.163.com/touch/all?ver=c&domains=163.com&display=mobile&product=3g_163";
+
     }
 }
